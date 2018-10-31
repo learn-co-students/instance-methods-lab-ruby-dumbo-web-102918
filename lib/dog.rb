9c03306616374a7451170f32bac9bb41acfa2025
gaboncio42 <cafecolau22@gmail.com>
2018-10-30 23:35:05 -0400

    Done

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog
+  #class body
+  #def class method bark
+  def bark
+    puts "Woof!"
+  end
+
+  #def class method sit
+  def sit
+    puts "The Dog is sitting"
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person
+  #class body
+
+  #define class method talk
+  def talk
+    puts "Hello World!"
+  end
+
+  #def class method walk
+  def walk
+    puts "The Person is walking"
+  end
+end
